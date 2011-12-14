d1571bda2c193391a58eecc0fc9179e92329a5f6
Guto Maia <guto@guto.net>
2011-12-14 17:53:51 -0200

    refactoring the test to able get_stats to be directly called. This is Python, not java\!

--- steamer/stats.py
+++ steamer/stats.py
@@ -1,15 +1,12 @@
-
-class StatsClient():
-
-    def get_stats(self, username, game):
-        return dict(
-            visibilityState = 3,
-            gameFriendlyName = 'TF2',
-            gameName = 'Team Fortress 2',
-            gameLink = 'http://store.steampowered.com/app/440',
-            gameIcon = 'http://media.steampowered.com/steamcommunity/public/images/apps/440/e3f595a92552da3d664ad00277fad2107345f743.jpg',
-            gameLogo = 'http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8.jpg',
-            gameLogoSmall = 'http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8_thumb.jpg',
-            steamID64 = 76561197985077150,
-            playerCustomURL = 'gutomaia'
-            )
+def get_stats(username, game):
+    return dict(
+        visibilityState = 3,
+        gameFriendlyName = 'TF2',
+        gameName = 'Team Fortress 2',
+        gameLink = 'http://store.steampowered.com/app/440',
+        gameIcon = 'http://media.steampowered.com/steamcommunity/public/images/apps/440/e3f595a92552da3d664ad00277fad2107345f743.jpg',
+        gameLogo = 'http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8.jpg',
+        gameLogoSmall = 'http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8_thumb.jpg',
+        steamID64 = 76561197985077150,
+        playerCustomURL = 'gutomaia'
+        )
--- tf2stats_test.py
+++ tf2stats_test.py
@@ -1,12 +1,11 @@
 import unittest
 
-from steamer.stats import StatsClient
+from steamer.stats import get_stats
 
 class TF2StatsTest(unittest.TestCase):
 
     def setUp(self):
-        stats_client = StatsClient()
-        self.stats = stats_client.get_stats("gutomaia", "tf2") #TODO implement with document later on
+        self.stats = get_stats("gutomaia", "tf2") #TODO implement with document later on
 
     def tearDown(self):
         self.stats = None;
