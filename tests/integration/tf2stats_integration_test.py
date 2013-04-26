be22f6b0b31bc5e0295b868ac0be987dfd497fc5
Guto Maia <guto@guto.net>
2013-04-26 01:20:40 -0300

    tf2 data now has integration tests

---
+++ tests/integration/tf2stats_integration_test.py
@@ -0,0 +1,23 @@
+from steamer.stats import get_stats, get_achievements
+
+from tf2stats_test import TF2StatsTest
+
+from integration.steam_simulator import SteamSimulator
+
+class TF2StatsIntegrationTest(TF2StatsTest):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sim = SteamSimulator()
+        cls.sim.start()
+        cls.stats = get_stats("gutomaia", "tf2")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.stats = None;
+        cls.sim.stop()
+        #while not cls.sim.stopped():
+        #    sleep(0.1)
+
+if __name__ == '__main__':
+    unittest.main()
